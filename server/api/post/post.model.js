d0e2bd3493d51c4594b4369a5f3be66b2d80511f
Renjie_Zhang <1114338218@qq.com>
2015-04-23 14:31:52 +0100

    user-server

--- server/api/post/post.model.js
+++ server/api/post/post.model.js
@@ -1,12 +1,31 @@
-'use strict';
-
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+        Schema = mongoose.Schema;
 
-var PostSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
-});
+    var CommentSchema = new Schema({
+        body: { type: String, required: true },
+        author: { type: String, required: true },
+        upvotes: Number
+      });
 
-module.exports = mongoose.model('Post', PostSchema);
+    var PostSchema = new Schema({
+      title: { type: String, required: true },
+      link: { type: String, optional: true },
+      username: { type: String, required: true },
+      comments: [CommentSchema],
+      upvotes: Number
+    });
+
+    module.exports = mongoose.model('posts', PostSchema);
+
+// 'use strict';
+
+// var mongoose = require('mongoose'),
+//     Schema = mongoose.Schema;
+
+// var PostSchema = new Schema({
+//   name: String,
+//   info: String,
+//   active: Boolean
+// });
+
+// module.exports = mongoose.model('Post', PostSchema);
